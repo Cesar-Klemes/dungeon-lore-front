502fb74625eec0804cc30d53cc7b00a0de098de8
Cesar Klemes <cesar.klemes@gmail.com>
2023-04-25 20:37:14 -0300

    Testando algumas coisas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row style="min-height: 850px">
       <v-col align-self="center">
         <v-row>
           <v-col cols="auto" class="d-flex justify-center align-center">
@@ -10,7 +10,7 @@
               height="235"
             />
           </v-col>
-          <v-col align-self="center" class="pl-0 align-center">
+          <v-col cols="auto" align-self="center" class="pl-0 align-center">
             <v-row>
               <v-col>
                 <h1 class="title">Dungeon Lore</h1>
@@ -23,26 +23,26 @@
             </v-row>
             <v-row>
               <v-col>
-                <v-btn class="bg-blue">Começar</v-btn>
+                <v-btn class="bg-blue" @click="start()">Começar</v-btn>
+                {{ grid }}
               </v-col>
             </v-row>
           </v-col>
         </v-row>
       </v-col>
-      <v-col cols="6" class="d-flex justify-center align-center">
-        <v-card class="pa-4 bg-blue-grey" max-width="500">
-          <p class="text-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod massa et augue tincidunt, quis bibendum eros bibendum. Nam eget sollicitudin enim. Donec elementum sapien auctor ipsum sollicitudin, in iaculis lectus malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sodales euismod orci, at bibendum magna ultrices quis. Sed vehicula erat in augue vehicula fringilla. In dictum nulla vel varius faucibus. Suspendisse potenti. Nulla ornare, est sit amet sollicitudin rutrum, elit lorem dictum quam, at bibendum metus nulla ac turpis. Praesent elementum est vitae felis bibendum, nec lacinia ante laoreet.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod massa et augue tincidunt, quis bibendum eros bibendum. Nam eget sollicitudin enim. Donec elementum sapien auctor ipsum sollicitudin, in iaculis lectus malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sodales euismod orci, at bibendum magna ultrices quis. Sed vehicula erat in augue vehicula fringilla. In dictum nulla vel varius faucibus. Suspendisse potenti. Nulla ornare, est sit amet sollicitudin rutrum, elit lorem dictum quam, at bibendum metus nulla ac turpis. Praesent elementum est vitae felis bibendum, nec lacinia ante laoreet.
-          </p>
-        </v-card>
+      <v-col v-if="grid" cols="6" class="d-flex justify-center align-center bg-blue-grey">
+
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-  //
+  let grid = false
+
+  function start(){
+    grid = true
+  }
 </script>
 <style>
 .title{
