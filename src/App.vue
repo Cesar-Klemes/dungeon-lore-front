aade070a46b814688706d00c184daa141a76587e
Cesar Klemes <cesar.klemes@gmail.com>
2023-04-25 22:43:04 -0300

    Adicionando imagem de fundo

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,38 @@
 <template>
-  <v-container>
-    <v-row style="min-height: 850px" class="text-center">
-      <v-col align-self="center">
-        <v-row>
-          <v-col>
-            <v-img
-              src="@/assets/logo-removebg.png"
-              width="225"
-              height="235"
-              class="mx-auto"
-            />
-            <h1 class="title">Dungeon Lore</h1>
-            <h4>Onde suas aventuras começam!</h4>
-            <v-btn
-              style="font-size: 20px; font-family: Georgia, serif"
-              color="#FF6600"
-              class="mt-5 text-capitalize"
-              variant="outlined"
-              @click="grid = true"
-            >
-              Começar
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-col>
-      <v-col v-show="grid" cols="6" class="bg-blue-grey">
+    <img
+      src="./assets/wallpaper.jpg"
+      class="wallpaper-background"
+    >
+    <v-container>
+      <v-row style="min-height: 850px" class="text-center">
+        <v-col align-self="center">
+          <v-row>
+            <v-col>
+              <v-img
+                src="@/assets/logo-removebg.png"
+                width="225"
+                height="235"
+                class="mx-auto"
+              />
+              <h1 class="title">Dungeon Lore</h1>
+              <h4>Onde suas aventuras começam!</h4>
+              <v-btn
+                style="font-size: 20px; font-family: Georgia, serif"
+                color="#FF6600"
+                class="mt-5 text-capitalize"
+                variant="outlined"
+                @click="grid = true"
+              >
+                Começar
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col v-show="grid" cols="6" class="bg-blue-grey">
 
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -45,6 +49,15 @@
 .title{
   color: #FF6600;
   font-family: "Times New Roman", serif;
+}
+
+.wallpaper-background {
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+  mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0));
 }
 
 body {
