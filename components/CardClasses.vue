fc6ceabcfc6c95384bf28284fe3a83eb6e0e052c
Cesar Klemes <cesar.klemes@gmail.com>
2023-05-17 13:31:37 -0300

    Adicionado Axios, incluindo requisição da DndApi e página de classes

--- components/CardClasses.vue
+++ components/CardClasses.vue
@@ -1,13 +1,28 @@
 <template>
-  <v-card>
-    Classe
-  </v-card>
+  <div>
+    <v-row>
+      <v-col v-for="classe in classes" cols="4">
+        <v-card min-height="200px" min-width="200px">
+          <v-card-title>{{ classe.name }}</v-card-title>
+          <v-divider/>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "CardClasses"
+  name: "CardClasses",
+
+  props: {
+    classes: {
+      type: Array,
+      default: () => [],
+    }
+  }
 }
+
 </script>
 
 <style scoped>
--- pages/classes/index.vue
+++ pages/classes/index.vue
@@ -1,14 +1,27 @@
 <template>
   <v-container>
     <v-row>
-      <CardClasses/>
+      <CardClasses :classes="classes"/>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import CardClasses from "@/components/CardClasses.vue";
+
 export default {
-  name: "index.vue"
+  name: "index.vue",
+
+  components: {
+    CardClasses,
+  },
+
+  async asyncData({ $axios }) {
+    const classes = await $axios.$get('https://www.dnd5eapi.co/api/classes')
+    return {
+      classes: classes.results,
+    }
+  }
 }
 </script>
 
