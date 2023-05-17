84141301b7017e4499451853d8d495d1e0fb803f
Cesar Klemes <cesar.klemes@gmail.com>
2023-05-17 14:24:57 -0300

    Adicionado lista de raças

---
+++ pages/races/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <CardRaces :races="races"/>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "index.vue",
+
+  async asyncData({ $axios }) {
+    const races = await $axios.$get('https://www.dnd5eapi.co/api/races')
+    return {
+      races: races.results,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
