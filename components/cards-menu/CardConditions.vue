49cc942e2969135011078f09c38f8b5ed1cb326a
Cesar Klemes <cesar.klemes@gmail.com>
2023-05-17 17:10:20 -0300

    Adicionado pagina de condições

---
+++ components/cards-menu/CardConditions.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-row>
+      <v-col v-for="condition in conditions" cols="4">
+        <v-card min-height="200px" min-width="200px">
+          <v-card-title>{{ condition.name }}</v-card-title>
+          <v-divider/>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardConditions",
+  props: {
+    conditions: {
+      type: Array,
+      default: () => [],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/conditions/index.vue
+++ pages/conditions/index.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    {{ conditions }}
-  </div>
+  <v-container>
+    <v-row>
+      <CardConditions :conditions="conditions"/>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import CardConditions from "~/components/cards-menu/CardConditions.vue";
+
 export default {
   name: "index.vue",
+  components: {CardConditions},
 
   async asyncData({ $axios }) {
     const conditions = await $axios.$get('https://www.dnd5eapi.co/api/conditions')
