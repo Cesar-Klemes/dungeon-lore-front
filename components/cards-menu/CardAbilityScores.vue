6baad2fe8eb08d753bc14bafc0fedca334581296
Cesar Klemes <cesar.klemes@gmail.com>
2023-05-25 17:09:38 -0300

    Adicionando informações a página de ability-scores

--- components/cards-menu/CardAbilityScores.vue
+++ components/cards-menu/CardAbilityScores.vue
@@ -3,8 +3,25 @@
     <v-row>
       <v-col v-for="ability in abilityScores" cols="4">
         <v-card min-height="200px" min-width="200px">
-          <v-card-title>{{ ability.name }}</v-card-title>
+          <v-card-title>{{ ability.full_name }}</v-card-title>
           <v-divider/>
+          <v-list>
+            <v-list-group prepend-item>
+              <template v-slot:activator>
+                <v-list-item-title>Description:</v-list-item-title>
+              </template>
+              <v-card-text v-for="desc in ability.desc"><v-list-item>{{ desc }}</v-list-item></v-card-text>
+            </v-list-group>
+            <v-list-group prepend-item>
+              <template v-slot:activator>
+                <v-list-item-title>Skills:</v-list-item-title>
+              </template>
+              <v-list-item v-for="skill in ability.skills">
+                <v-btn width="100%">{{ skill.name }}</v-btn>
+              </v-list-item>
+              <v-list-item><span v-if="!ability.skills.length">No Skills</span></v-list-item>
+            </v-list-group>
+          </v-list>
         </v-card>
       </v-col>
     </v-row>
@@ -14,13 +31,12 @@
 <script>
 export default {
   name: "CardAbilityScores",
-
   props: {
     abilityScores: {
       type: Array,
       default: () => [],
-    }
-  }
+    },
+  },
 }
 </script>
 
--- pages/ability-scores/index.vue
+++ pages/ability-scores/index.vue
@@ -15,10 +15,28 @@ export default {
 
   async asyncData({ $axios }) {
     const abilityScores = await $axios.$get('https://www.dnd5eapi.co/api/ability-scores')
+    let scoreDetail = []
+
+    for (const score of abilityScores.results) {
+      let detail = await $axios.$get('https://www.dnd5eapi.co/api/ability-scores/' + score.index)
+      scoreDetail.push(detail)
+    }
+
+    for (let i = 0; i < abilityScores.results.length; i++) {
+      let score = abilityScores.results[i];
+      let detail = scoreDetail.find(detail => detail.index === score.index);
+      if (detail) {
+        score.full_name = detail.full_name;
+        score.desc = detail.desc;
+        score.skills = detail.skills;
+      }
+    }
+
     return {
       abilityScores: abilityScores.results,
     }
   }
+
 }
 </script>
 
