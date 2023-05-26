4f45277f7f44dd9506a52cabf296ec44ed079630
Cesar Klemes <cesar.klemes@gmail.com>
2023-05-26 16:20:55 -0300

    Adicionado conteudo da página languages

--- components/cards-menu/CardLanguages.vue
+++ components/cards-menu/CardLanguages.vue
@@ -5,6 +5,17 @@
         <v-card min-height="200px" min-width="200px">
           <v-card-title>{{ language.name }}</v-card-title>
           <v-divider/>
+          <v-card-title>Type</v-card-title>
+          <v-card-subtitle>{{ language.type }}</v-card-subtitle>
+          <v-card-title>Typical Speakers</v-card-title>
+          <v-row class="ml-1 mt-n6">
+            <v-col cols="auto" v-for="speaker in language.typical_speakers">
+              <v-chip color="cyan" outlined>{{ speaker }}</v-chip>
+            </v-col>
+          </v-row>
+          <v-card-title>Alphabet</v-card-title>
+          <v-card-subtitle v-if="language.script">{{ language.script }}</v-card-subtitle>
+          <v-card-subtitle v-else-if="!language.script"><span> No Alphabet</span></v-card-subtitle>
         </v-card>
       </v-col>
     </v-row>
--- pages/languages/index.vue
+++ pages/languages/index.vue
@@ -15,6 +15,23 @@ export default {
 
   async asyncData({ $axios }) {
     const languages = await $axios.$get('https://www.dnd5eapi.co/api/languages')
+    let language = []
+
+    for (const value of languages.results) {
+      let detail = await $axios.$get('https://www.dnd5eapi.co/api/languages/' + value.index)
+      language.push(detail)
+    }
+
+    for (let i = 0; i < languages.results.length; i++) {
+      let value = languages.results[i];
+      let detail = language.find(detail => detail.index === value.index);
+      if (detail) {
+        value.type = detail.type;
+        value.typical_speakers = detail.typical_speakers;
+        value.script = detail.script;
+      }
+    }
+
     return {
       languages: languages.results,
     }
