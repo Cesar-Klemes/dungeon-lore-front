71688848d89d92e7665d5f51f20191b48bf8b73b
Cesar Klemes <cesar.klemes@gmail.com>
2023-05-26 15:53:29 -0300

    Criado o conteudo da página alignments

--- components/cards-menu/CardAlignments.vue
+++ components/cards-menu/CardAlignments.vue
@@ -2,9 +2,12 @@
   <div>
     <v-row>
       <v-col v-for="alignment in alignments" cols="4">
-        <v-card min-height="200px" min-width="200px">
+        <v-card min-height="270px" min-width="200px">
           <v-card-title>{{ alignment.name }}</v-card-title>
           <v-divider/>
+          <v-card-title>Abbreviation</v-card-title>
+          <v-card-subtitle>({{ alignment.abbreviation }})</v-card-subtitle>
+          <v-card-text>{{ alignment.desc }}</v-card-text>
         </v-card>
       </v-col>
     </v-row>
--- pages/alignments/index.vue
+++ pages/alignments/index.vue
@@ -15,6 +15,22 @@ export default {
 
   async asyncData({ $axios }) {
     const alignments = await $axios.$get('https://www.dnd5eapi.co/api/alignments')
+    let alignment = []
+
+    for (const value of alignments.results) {
+      let detail = await $axios.$get('https://www.dnd5eapi.co/api/alignments/' + value.index)
+      alignment.push(detail)
+    }
+
+    for (let i = 0; i < alignments.results.length; i++) {
+      let value = alignments.results[i];
+      let detail = alignment.find(detail => detail.index === value.index);
+      if (detail) {
+        value.abbreviation = detail.abbreviation;
+        value.desc = detail.desc;
+      }
+    }
+
     return {
       alignments: alignments.results,
     }
