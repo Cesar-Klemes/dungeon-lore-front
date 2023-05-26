98963df3b2a842a09fd2e24c948c205a44d634be
Cesar Klemes <cesar.klemes@gmail.com>
2023-05-26 15:01:33 -0300

    Alterando design na navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,22 +6,56 @@
       :clipped="clipped"
       fixed
       app
+      style="width: auto"
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item to="/">
           <v-list-item-action>
-            <v-icon :class="{ 'red--text': item.icon === 'mdi-book-open-page-variant' }">{{ item.icon }}</v-icon>
+            <v-icon class="red--text">mdi-book-open-page-variant</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title :class="{ 'red--text': item.title === 'The Spellbook' }">{{ item.title }}</v-list-item-title>
+            <v-list-item-title class="red--text">The Spellbook</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+
+        <v-list-group>
+          <template v-slot:activator>
+            <v-icon>mdi-human</v-icon>
+            <v-list-item-title class="pl-8">Character Data</v-list-item-title>
+          </template>
+          <v-list-item
+            v-for="(item, i) in itemsCharacterData"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+          >
+            <v-list-item-action :class="{ 'ml-6': !miniVariant }">
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content class="ml-n4">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+
+
+
+
+<!--        <v-list-item-->
+<!--          v-for="(item, i) in items"-->
+<!--          :key="i"-->
+<!--          :to="item.to"-->
+<!--          router-->
+<!--          exact-->
+<!--        >-->
+<!--          <v-list-item-action>-->
+<!--            <v-icon :class="{ 'red&#45;&#45;text': item.icon === 'mdi-book-open-page-variant' }">{{ item.icon }}</v-icon>-->
+<!--          </v-list-item-action>-->
+<!--          <v-list-item-content>-->
+<!--            <v-list-item-title :class="{ 'red&#45;&#45;text': item.title === 'The Spellbook' }">{{ item.title }}</v-list-item-title>-->
+<!--          </v-list-item-content>-->
+<!--        </v-list-item>-->
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
@@ -96,12 +130,7 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-book-open-page-variant',
-          title: 'The Spellbook',
-          to: '/'
-        },
+      itemsCharacterData: [
         {
           icon: 'mdi-numeric-9-plus-box-multiple-outline',
           title: 'Ability Scores',
@@ -111,6 +140,28 @@ export default {
           icon: 'mdi-drama-masks',
           title: 'Alignments',
           to: '/alignments'
+        },
+        {
+          icon: 'mdi-script-text',
+          title: 'Languages',
+          to: '/languages'
+        },
+        {
+          icon: '',
+          title: 'Proficiencies',
+          to: '/'
+        },
+        {
+          icon: '',
+          title: 'Skills',
+          to: '/'
+        },
+      ],
+      items: [
+        {
+          icon: 'mdi-book-open-page-variant',
+          title: 'The Spellbook',
+          to: '/'
         },
         {
           icon: 'mdi-bottle-tonic-skull',
@@ -141,11 +192,6 @@ export default {
           icon: 'mdi-script-text-outline',
           title: 'Features',
           to: '/features'
-        },
-        {
-          icon: 'mdi-script-text',
-          title: 'Languages',
-          to: '/languages'
         },
         {
           icon: 'mdi-magic-staff',
